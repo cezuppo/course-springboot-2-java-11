fe93acae1ea9ff07151e266794c62b3c3d4fd3c3
cezuppo <czuppo@gmail.com>
2020-12-07 06:30:24 -0300

    Exception handling - delete

--- src/main/java/com/example/course/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/example/course/services/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,6 @@
 package com.example.course.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-
 	private static final long serialVersionUID = 1L;
 
 	public ResourceNotFoundException(Object id) {
